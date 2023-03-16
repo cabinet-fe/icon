538204607f2ce413448fcea6fc69058b305eaa6c
whj <emailwen@qq.com>
2023-03-16 16:09:03 +0800

    docs: [ci]

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,19 +1,24 @@
 <template>
-  <component :is="Icon" v-for="(Icon, key) in icons" :key="key" class="icon" />
-  <!-- <hr />
-  <component
-    :is="`ElIcon${key}`"
-    v-for="key in Object.keys(icons)"
-    :key="key"
-    class="icon"
-  /> -->
+  <span v-for="(Icon, key) in icons" :key="key" class="icon-box">
+    <component :is="Icon"  class="icon" />
+    <div >
+      {{ key }}
+    </div>
+  </span>
 </template>
 
 <script lang="ts" setup>
-import { icons } from 'icon-ultra/global'
+import { icons } from 'icon-ultra/vue'
 </script>
 
 <style>
+.icon-box {
+  display: inline-block;
+  width: 120px;
+  height: 80px;
+  font-size: 14px; text-align: center;
+}
+
 .icon {
   height: 48px;
   color: #409eff;
